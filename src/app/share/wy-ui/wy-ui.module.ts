c5a59af0d2fbbce1c60deb2d948bd496a132c0ca
YZY <yzy@luodedao.com>
2022-06-27 17:21:02 +0800

    新增主页下方静态播放器（进度条下次实现）

--- src/app/share/wy-ui/wy-ui.module.ts
+++ src/app/share/wy-ui/wy-ui.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SingleSheetComponent } from './single-sheet/single-sheet.component';
 import { PlayCountPipe } from '../play-count.pipe';
+import { WyPlayerModule } from './wy-player/wy-player.module';
+import { WyPlayerComponent } from './wy-player/wy-player.component';
 
 
 
@@ -11,10 +13,12 @@ import { PlayCountPipe } from '../play-count.pipe';
     PlayCountPipe,
   ],
   imports: [
+    WyPlayerModule
   ],
   exports: [
     SingleSheetComponent,
     PlayCountPipe,
+    WyPlayerModule
   ]
 })
 export class WyUiModule { }
